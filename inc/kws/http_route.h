69028fe684945179a00e82d35eb3e9f4f59f9bdf
keelerjr12 <Joshua Keeler>
2024-03-03 22:02:33 -0700

    More clang format fixes

--- inc/kws/http_route.h
+++ inc/kws/http_route.h
@@ -1,47 +1,52 @@
 #ifndef HTTP_ROUTE_H
 #define HTTP_ROUTE_H
 
-#include <string>
 #include <kws/http_method.h>
+#include <string>
 
 namespace KWS {
 
-  class HttpRoute {
-   public:
+class HttpRoute
+{
+  public:
+    HttpRoute(HttpMethod method, const std::string& path)
+      : method_(method)
+      , path_(path)
+    {}
 
-    HttpRoute(HttpMethod method, const std::string& path) : method_(method), path_(path) { }
-
-    HttpMethod Method() const {
-      return method_;
+    HttpMethod Method() const
+    {
+        return method_;
     }
 
-    std::string Path() const {
-      return path_;
+    std::string Path() const
+    {
+        return path_;
     }
 
-   private:
+  private:
     HttpMethod method_;
     std::string path_;
-  };
+};
 
-  bool operator==(const HttpRoute& lhs, const HttpRoute& rhs);
+bool operator==(const HttpRoute& lhs, const HttpRoute& rhs);
 
-}
+}  // namespace KWS
 
 namespace std {
 
-  template <>
-  struct hash<KWS::HttpRoute> {
+template<>
+struct hash<KWS::HttpRoute>
+{
+    size_t operator()(const KWS::HttpRoute& route) const
+    {
+        const auto method_hash_fn = hash<int>{};
+        const auto uri_hash_fn = hash<std::string>{};
 
-    size_t operator()(const KWS::HttpRoute& route) const {
-      const auto method_hash_fn = hash<int>{};
-      const auto uri_hash_fn = hash<std::string>{};
-      
-      return method_hash_fn((int)route.Method()) ^ uri_hash_fn(route.Path());
+        return method_hash_fn((int)route.Method()) ^ uri_hash_fn(route.Path());
     }
+};
 
-  };
+}  // namespace std
 
-}
-
-#endif // HTTP_ROUTE_H
+#endif  // HTTP_ROUTE_H
