3de7af3296b9ac2ccdc08128a9c18eb540180912
Joshua Keeler <Joshua Keeler>
2024-03-03 21:55:09 -0700

    Clang format fixes

--- inc/kws/http_method.h
+++ inc/kws/http_method.h
@@ -5,19 +5,20 @@
 
 namespace KWS {
 
-  enum class HttpMethod {
+enum class HttpMethod
+{
     GET,
     POST,
     PUT,
     DELETE
-  };
+};
 
-  // TODO: move this to a utility class
-  std::string toUpper(std::string str);
+// TODO: move this to a utility class
+std::string toUpper(std::string str);
 
-  HttpMethod ToHttpMethod(const std::string& str);
-  std::string ToString(HttpMethod method);
+HttpMethod ToHttpMethod(const std::string& str);
+std::string ToString(HttpMethod method);
 
-}
+}  // namespace KWS
 
-#endif // HTTP_METHOD_H
+#endif  // HTTP_METHOD_H
